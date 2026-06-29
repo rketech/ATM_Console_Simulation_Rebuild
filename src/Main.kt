1e8803d7d7fc4e0227067ef8404bc2b56a4711cb
rketech <122847077+rketech@users.noreply.github.com>
2026-06-29 08:48:02 +0530

    Initial Commit 1. Created Permanent Variables 2. Created Login Attempt Counter 3. Validated Entered PIN towards stored PIN 4. Used Thread.sleep() for User Experience 5. Proper business flow

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,56 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    /*     Permanent Values     */
+    val customerName: String = "Rakesh Sharma"
+    val accountNumber: String = "AC001"
+    val pin: String = "0123"
+    val balance: Double = 25000.0
+    val phoneNumber: String = "9797979797"
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    /* ATM Operation */
+    println("")
+    println("=================================")
+    println("       Rakesh Bank ATM           ")
+    println("=================================")
+    println("")
+
+    Thread.sleep(1000)
+
+    println("Welcome! Please enter your ATM card...")
+
+    Thread.sleep(1000)
+
+    var loginAttempt = 0
+
+    do {
+        println("")
+        Thread.sleep(1000)
+
+        print("Please enter your 4-Digit PIN: ")
+        val enteredPin: String? = readlnOrNull()
+
+        Thread.sleep(1000)
+        println("")
+
+        if (enteredPin.isNullOrBlank()) {
+            println("PIN cannot be empty")
+            loginAttempt++
+        } else if (enteredPin == pin) {
+            println("PIN verified successfully")
+            return
+        } else {
+            println("Incorrect PIN")
+            loginAttempt++
+            println("Remaining Attempts: ${3 - loginAttempt}")
+        }
+    } while (loginAttempt < 3)
+
+    Thread.sleep(1000)
+
+    println("")
+
+    println("Account Locked!")
+
+    Thread.sleep(1000)
+
+    println("Exiting ATM...")
 }
